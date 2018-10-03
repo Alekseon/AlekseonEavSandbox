de60e8ea8248d3fba424a39fa84bd660eb6c6f2d
marcin.frymark <m.frymark@alekseon.com>
2018-10-03 05:13:21 +0200

    change for task-27

--- Block/Adminhtml/Attribute/Grid.php
+++ Block/Adminhtml/Attribute/Grid.php
@@ -14,18 +14,13 @@ use Alekseon\AlekseonEav\Block\Adminhtml\Attribute\Grid as EavGrid;
 class Grid extends EavGrid
 {
     /**
-     * @var \Alekseon\AlekseonEavSandbox\Model\ResourceModel\Attribute\CollectionFactory
-     */
-    protected $collectionFactory;
-
-    /**
      * Grid constructor.
      * @param \Magento\Backend\Block\Template\Context $context
      * @param \Magento\Backend\Helper\Data $backendHelper
      * @param \Magento\Config\Model\Config\Source\Yesno $yesNoSource
      * @param \Alekseon\AlekseonEav\Model\Adminhtml\System\Config\Source\Scopes $scopesSource
-     * @param \Alekseon\AlekseonEavSandbox\Model\ResourceModel\Attribute\CollectionFactory $collectionFactory
      * @param \Alekseon\AlekseonEav\Model\Adminhtml\System\Config\Source\InputType $inputTypeSource
+     * @param \Alekseon\AlekseonEavSandbox\Model\ResourceModel\Attribute\CollectionFactory $collectionFactory
      * @param array $data
      */
     public function __construct(
@@ -33,24 +28,10 @@ class Grid extends EavGrid
         \Magento\Backend\Helper\Data $backendHelper,
         \Magento\Config\Model\Config\Source\Yesno $yesNoSource,
         \Alekseon\AlekseonEav\Model\Adminhtml\System\Config\Source\Scopes $scopesSource,
-        \Alekseon\AlekseonEavSandbox\Model\ResourceModel\Attribute\CollectionFactory $collectionFactory,
         \Alekseon\AlekseonEav\Model\Adminhtml\System\Config\Source\InputType $inputTypeSource,
+        \Alekseon\AlekseonEavSandbox\Model\ResourceModel\Attribute\CollectionFactory $collectionFactory,
         array $data = []
     ) {
-        $this->collectionFactory = $collectionFactory;
-        parent::__construct($context, $backendHelper, $yesNoSource, $scopesSource, $inputTypeSource, $data);
-    }
-
-    /**
-     * Prepare product attributes grid collection object
-     *
-     * @return $this
-     */
-    protected function _prepareCollection()
-    {
-        $collection = $this->collectionFactory->create();
-        $this->setCollection($collection);
-
-        return parent::_prepareCollection();
+        parent::__construct($context, $backendHelper, $yesNoSource, $scopesSource, $inputTypeSource, $collectionFactory, $data);
     }
 }
